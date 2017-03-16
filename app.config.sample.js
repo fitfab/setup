221790d2873bcb595710b00eebf2bf4901f3ce4f
Miguel Julio <fitfab@yahoo.com>
2017-03-16 11:36:07 -0400

    add priavte config for the app and set node env for dev & prod

---
+++ app.config.sample.js
@@ -0,0 +1,13 @@
+/**
+ * Holds appilcation variables such as
+ * DB URI
+ * User and Password information
+ *
+ * Note:    after cloning this repo save file as "app.config.js"
+ *          and update your database information and any other
+ *          private information.
+ * @type {Object}
+ */
+module.exports = {
+    db_uri: 'mongodb://user:password@ds0072017.mlab.com:port/database'
+}
